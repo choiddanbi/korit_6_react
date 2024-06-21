5791b59f0dd9b20d56b0ad5e8d09122002525399
choiddanbi <eksql5955@naver.com>
2024-06-21 17:38:14 +0900

    240621

--- src/ch10_3/App.js
+++ src/ch10_3/App.js
@@ -1,14 +1,23 @@
 import { useRef, useState } from "react";
 import "./App.css";
+import Swal from "sweetalert2";
 
 function App() {
+
+   /* const test = {
+        a: "aaa",
+        b: "bbb"
+    }
+    console.log(test.a); // aaa
+    console.log(test["a"]); // aaa*/
+
     const emptyUser = {
         username: "",
         password: "",
         name: ""
     }
     const [ inputData, setInputData ] = useState({...emptyUser});
-    const [ userList, setUserList ] = useState([]);
+    const [ userList, setUserList ] = useState([]); // useState >> set으로 userList 값을 변경해서 새로운 배열로 리턴해라!
 
     const inputRef = {
         username: useRef(),
@@ -46,6 +55,69 @@ function App() {
         });
     }
 
+    // 수정은 모달창을 써야하는데 걍 swal 사용~
+    const handleEditClick = (key, index) => {
+        Swal.fire({
+            // console.log(e); // td태그는 target 에 name, value 없음
+            // console.log(key);
+            // console.log(index);
+            title: `${key} edit`,
+            input: "text",
+            inputValue: userList[index][key], // 객체[index][key값] >> 객체의 키값으로 value 꺼낸다??????? userList의 index 위치의 key위치의 값
+            showCancelButton: true, 
+            cancelButtonText: "취소",
+            confirmButtonText: "확인"
+        }).then(result =>  {
+            if(result.isConfirmed) { // confirmed 가 true면 (확인을 누르면) 수정!!! user, i >> 변수인데 user는 'UserList의 value = user객체'이고 i는 'UserList의 index' >> userList배열의 인덱스에 위치하는 값 >> 배열.map((value, index)=> {}) >> value와 index로 함수 돌려서 새로운 배열ㅇㅔ 담기
+                setUserList(userList => [...userList.map((user, i) => { // index => onclick 에서의 userList index 내가 선택한 우ㅣ치,, i는 userList.map 안에서 순서대로
+                    if( i === index ){
+                        return { // index 가 같은 애만 수정
+                            ...user,
+                            [key]: result.value
+                        }
+                    }
+                    return user;
+                }) ]);
+             //console.log(result);
+            }
+        });    
+    }
+
+
+    const handleDeleteClick = (e) => {
+        Swal.fire({
+            title: "사용자 삭제",
+            text: "해당 사용자를 삭제하시겠습니까?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonText: "삭제",
+            confirmButtonColor: "red",
+            cancelButtonText: "취소"
+    }).then(result => {
+        //console.log(result); // 
+        if(result.isConfirmed) {
+            setUserList( userList => [ ...userList.filter((user, index) => index !== parseInt(e.target.value)) ])
+        } 
+    });
+}
+
+    /*
+    UserList 는
+
+    배열.map((value, index) => { return })  // 배열안의 값들을 모두 함수 돌려서 새로운 배열에 옮겨 
+      -> ex) userList 에 value 는 user 객체
+    
+    배열.filter((value, index) => { return })  // 배열안의 값들 중 조건에 맞는 거만 새로운 배열에 옮겨
+     * /
+
+
+    /*const handleDeleteClick = (e) => {
+        if(window.confirm("해당 사용자를 삭제하시겠습니까?")) {
+            setUserList( userList => [ ...userList.filter((user, index) => index !== parseInt(e.target.value)) ]) 
+        }
+    }*/
+
+
 
     return <>
         {/*
@@ -81,6 +153,8 @@ function App() {
                     <th>username</th>
                     <th>password</th>
                     <th>name</th>
+                    <th>edit</th>
+                    <th>delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -89,9 +163,15 @@ function App() {
                     return (
                         <tr key={index}>
                             <td>{index + 1}</td>
-                            <td>{username}</td>
-                            <td>{password}</td>
-                            <td>{name}</td>
+                            <td onClick={() => handleEditClick("username", index)}>{username}</td> {/*handleEditClick 이 일어났을때 username괴 index를 가져올건데 함수를 호출해서!! () => 어저꾸 */ }
+                            <td onClick={() => handleEditClick("password",index)}>{password}</td>
+                            <td onClick={() => handleEditClick("name", index)}>{name}</td>
+                            <td>
+                                <button value={index}>edit</button> 
+                            </td>
+                            <td>
+                                <button onClick={handleDeleteClick} value={index}>delete</button> {/*얘는 함수 정의*/}
+                            </td>
                         </tr>
                     );
                 })
