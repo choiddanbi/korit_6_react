debfba1854644da4736f4d05805502fcca39ed5b
choiddanbi <eksql5955@naver.com>
2024-06-24 17:56:07 +0900

    2024-06-24

--- src/ch11/components/DataTable/DataTable.jsx
+++ src/ch11/components/DataTable/DataTable.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataTableBody from "../DataTableBody/DataTableBody";
 import DataTableHeader from "../DataTableHeader/DataTableHeader";
 import "./style.css";
@@ -7,11 +7,25 @@ import { SAMPLE_PRODUCTS } from "../../constants/sampleProducts";
 function DataTable() {
     const [ mode, setMode ] = useState(0); // 0 = 조회, 1 = 추가, 2 = 수정, 3 = 삭제
     const [ products, setProducts ] = useState([ ...SAMPLE_PRODUCTS]);
+    const [ isDeleting, setDeleting ] = useState(false);
+
+
+    // 처음 페이지가 열렸을 때 저장소에 값이 없으면 [], 있으면 그걸 들고옴
+    useEffect(()=> {
+        const lsProducts = localStorage.getItem("products");
+        setProducts(!lsProducts ? [] : JSON.parse(lsProducts) );
+    }, []);
+
+
+    // 저장소에 저장해주기 위해 ( products가 변했을때ㅐ)
+    useEffect( () => {
+        localStorage.setItem("products", JSON.stringify(products));
+    },[products]);
 
     return ( 
         <div className="table-main-container">
-            <DataTableHeader mode={mode} setMode={setMode} />
-            <DataTableBody mode={mode} products={products} />
+            <DataTableHeader mode={mode} setMode={setMode} setProducts={setProducts} setDeleting={setDeleting} />
+            <DataTableBody mode={mode} setMode={setMode} products={products} setProducts={setProducts} isDeleting={isDeleting} setDeleting={setDeleting} /> 
         </div>
      );
 }
--- src/ch11/components/DataTableHeader/DataTableHeader.jsx
+++ src/ch11/components/DataTableHeader/DataTableHeader.jsx
@@ -1,23 +1,92 @@
+import { useRef, useState } from "react";
 import "./style.css";
+import Swal from "sweetalert2";
 
 
-function DataTableHeader({ mode, setMode }) {
+function DataTableHeader({ mode, setMode , setProducts, setDeleting }) {
 
+    const emptyProduct = {
+        id: "",
+        productName: "",
+        size: "",
+        color: "",
+        price: ""
+    };
+
+    const inputRefs = {
+        productName : useRef(),
+        size: useRef(),
+        color: useRef(),
+        price: useRef()
+    };
+
+    const [ inputData, setInputData ] = useState({...emptyProduct});
+
+
+    const handleInputChange = (e) => {
+        setInputData(inputData => ({ // 객체 리턴
+            ...inputData,
+            [e.target.name]: e.target.value // input 창에 있는 값들을 받아옴
+        }));
+    }
+
+    const handleInputKeyDown = (e) => {
+        if(e.keyCode === 13 ) {
+            if(e.target.name === "productName") {
+                inputRefs.size.current.focus();
+            }
+            if(e.target.name === "size") {
+                inputRefs.color.current.focus();
+            }
+            if(e.target.name === "color") {
+                inputRefs.price.current.focus();
+            }
+            if(e.target.name === "price") {
+                handleSubmitClick();
+                inputRefs.productName.current.focus();
+            }
+        }
+    }
     const handleChangeModoClick = (e) => {
         setMode(parseInt(e.target.value));
     }
 
     const handleSubmitClick = () => {
         if(mode === 1) {
-            alert("상품추가")
+            setProducts(products => {
+                const productIds = products.map(product => product.id); // id값만 가져옴
+                const maxId = productIds.length === 0 
+                    ? 0 
+                    : Math.max.apply(null, productIds); // product 추가 할 때 마다 새로운 id 값 부여
+
+                return [ ...products, { ...inputData, id : maxId + 1 }];
+            });
+            Swal.fire({
+                title:"상품 정보 추가 완료",
+                icon:"success",
+                position: "top=end",
+                showConfirmButton: false,
+                timer: 500
+            });
+            resetMode();
         }
         if(mode === 2) {
-            alert("상품수정")
+            alert("상품수정");
         }
         if(mode === 3) {
-            alert("상품삭제")
+            Swal.fire({
+                title: "상품 정보 삭제",
+                test:"정말로 삭제 하시겠습니까 ?",
+                showCancelButton: true,
+                confirmButtonText: "삭제",
+                confirmButtonColor: "red",
+                cancelButtonText: "취소"
+            }).then(result => {
+                if(result.isConfirmed) {
+                    setDeleting(true);
+                }
+            });
         }
-        resetMode();
     }
 
     const handleCancelClick = () => {
@@ -26,16 +95,53 @@ function DataTableHeader({ mode, setMode }) {
 
     const resetMode = () => {
         setMode(0);
-
+        setInputData({ ...emptyProduct });
     }
 
     return ( 
         <header className="table-header">
             <div className="input-group">
-                <input type="text" disabled={mode === 0 || mode === 3} placeholder="상품명" autoFocus />
-                <input type="text" disabled={mode === 0 || mode === 3} placeholder="사이즈" />
-                <input type="text" disabled={mode === 0 || mode === 3} placeholder="색상" />
-                <input type="text" disabled={mode === 0 || mode === 3} placeholder="가격" />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="productName"
+                    value={inputData.productName}
+                    placeholder="상품명" 
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                    ref={inputRefs.productName}
+                    autoFocus 
+                />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="size"
+                    value={inputData.size}
+                    placeholder="사이즈"
+                    ref={inputRefs.size}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="color"
+                    value={inputData.color}
+                    placeholder="색상"
+                    ref={inputRefs.color}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                />
+                <input 
+                    type="text" 
+                    disabled={mode === 0 || mode === 3} 
+                    name="price"
+                    value={inputData.price}
+                    placeholder="가격" 
+                    ref={inputRefs.price}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                />
             </div>
             <div>
                 {
